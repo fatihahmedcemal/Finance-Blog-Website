d3f136a3561581d7792b4bf98110fafb835a2898
Ahmed Bozdogan <cj@MacBook-Pro-6.local>
2024-10-10 15:13:41 -0700

    Bug Fix.

---
+++ Desktop/Finance-Blog-Website/schemas/adminUser.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const adminUserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("Admin Users", adminUserSchema);
